d7d5a946c2bb98f80ee0a64e462eef7328dfe03a
Minh Dang Pham <minh.pham.024@pslib.cz>
2025-05-09 18:16:50 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,52 +1,49 @@
-radio.setGroup(11);
+radio.setGroup(11)
 
-let x: number = 0;
-let y: number = 0;
-
-radio.onReceivedValue(function (name, value) {
-    if (name == "x") {
-        x = value;
-    } else if (name == "y") {
-        y = value;
-    }
-
-    controlServo(x, y);
-})
-radio.onReceivedString(function (string){
-    if (string == "Stop"){
-    PCAmotor.MotorStopAll()
-    basic.pause(1000)
-    }
-})
+let y = 0
+let x = 0
+let turn = 0
+let speed = 0
 
 function controlServo(xTilt: number, yTilt: number) {
-    let speed = Math.map( yTilt, -1023, 1023, -200, 200)
-    speed = Math.constrain(speed, 0, 200);
-
-    let turn = Math.map(xTilt, -1023, 1023, -200, 200)
-    turn = Math.constrain(turn, 0, 200);
-
-   
+    speed = Math.map(yTilt, -1023, 1023, -200, 200)
+    speed = Math.constrain(speed, 0, 200)
+    turn = Math.map(xTilt, -1023, 1023, -200, 200)
+    turn = Math.constrain(turn, 0, 200)
     if (yTilt < -10) {
-        //vpřed
-        PCAmotor.MotorRun(PCAmotor.Motors.M4, (yTilt + (xTilt/2)))
-        PCAmotor.MotorRun(PCAmotor.Motors.M1, (yTilt - (xTilt/2)))
+        // vpřed
+        PCAmotor.MotorRun(PCAmotor.Motors.M4, yTilt + xTilt / 2)
+        PCAmotor.MotorRun(PCAmotor.Motors.M1, yTilt - xTilt / 2)
     } else if (yTilt > 10) {
-        //pozpátku
+        // pozpátku
         PCAmotor.MotorRun(PCAmotor.Motors.M1, yTilt)
         PCAmotor.MotorRun(PCAmotor.Motors.M4, yTilt)
     } else {
-        //stop
+        // stop
         PCAmotor.MotorStopAll()
     }
-/*
-    if (xTilt < -10) {
-        // zatočit do leva
-        PCAmotor.MotorRun(PCAmotor.Motors.M1, (xTilt - 50))
-        PCAmotor.MotorRun(PCAmotor.Motors.M4, xTilt)
-    } else if (xTilt > 10) {
-        // zatočit do prava
-        PCAmotor.MotorRun(PCAmotor.Motors.M1, xTilt)
-        PCAmotor.MotorRun(PCAmotor.Motors.M4, (xTilt - 50))
-    }*/
 }
+radio.onReceivedValue(function (name, value) {
+    if (name == "x") {
+        x = value
+    } else if (name == "y") {
+        y = value
+    }
+    controlServo(x, y)
+})
+radio.onReceivedString(function (string) {
+    if (string == "Stop") {
+        PCAmotor.MotorStopAll()
+        basic.pause(1000)
+    }
+})
+
+// if (xTilt < -10) {
+// // zatočit do leva
+// PCAmotor.MotorRun(PCAmotor.Motors.M1, (xTilt - 50))
+// PCAmotor.MotorRun(PCAmotor.Motors.M4, xTilt)
+// } else if (xTilt > 10) {
+// // zatočit do prava
+// PCAmotor.MotorRun(PCAmotor.Motors.M1, xTilt)
+// PCAmotor.MotorRun(PCAmotor.Motors.M4, (xTilt - 50))
+// }
